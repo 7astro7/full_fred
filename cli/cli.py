d6062b14442b53e9a9717d1912818b79a94b2f3c
7astro7 <zaknyy@protonmail.com>
2021-03-15 16:51:38 -0700

    adds cli module init

---
+++ cli/cli.py
@@ -0,0 +1,11 @@
+
+from fredapi import Fred
+from docopt import docopt
+
+# use docopt to parse command-line arguments
+# add tests for each module
+# logging?
+# set up configuration
+# generate fredapi docs in this module so docopt has something to map cli arguments to fredapi parameters with
+
+fred = Fred()
