7620013507390074d77623b81626cdbbd27f38d2
7astro7 <zaknyy@protonmail.com>
2021-03-23 17:26:06 -0700

    begins tailoring api requests to cli (subclassing fredapi.Fred)

--- fredcli/cli.py
+++ fredcli/cli.py
@@ -63,11 +63,20 @@ Arguments:
 
 """
 
+# determine whether writing an api / fred class from scratch is auspicous:
+#   the methods in fredapi might not necessarily be the optimal methods
+#   of all that fred's api can support (reference site, gauge from there)
+#       - categories, releases, series, tags, sources can all be classes potentially
+# need to subclass Fred: 
+#   _fetch_data() and get_series_info() need to be modified to raise 
+#   exceptions with minimal output to terminal
+# make sure all comands are filtered in cli function
 # it may be prudent to ask for user configuration on first use 
 #   this can include whether to save series by default in cwd
 # order usage alphabetically by command
 # order commands alphabetically 
 # note defaults in __doc__
+# add methods not included in fredapi
 # create option for **kwargs
 # help $ command for a specific command
 # add tests for each module
@@ -89,6 +98,9 @@ Arguments:
 
 from .constants import VERSION
 from fredapi import Fred
+from fredapi.fred import (
+        urlopen, ET, pd, HTTPError,
+        )
 from docopt import docopt
 import sys
 import os
@@ -106,58 +118,39 @@ def main():
 
 def cli():
     """
-    Run command sent as user input
+    Run commands sent as user input
     """
 
     args = docopt(__doc__, version = VERSION) # save version as a constant. reference Python versioning system
                                             # may make sense to base version on fredapi 
 
-    print(args)
+    print(args, type(args))
 
     if args.get("a") or args.get("all"):
-        if not args.get("series-id"):
-            pass
+        check_id_param(args)
 
     elif args.get("f") or args.get("first"):
-        if not args.get("<series-id>"):
-            pass
+        check_id_param(args)
 
     elif args.get("i") or args.get("info"):
-        if not args.get("<series-id>"):
-            print("No series_id passed")
-            sys.exit(1)
+        check_id_param(args, "<series-id>")
         # try / except
         series_id = args.get("<series-id>").upper()
-        series_info = Fred().get_series_info(series_id)
+        series_info = FredCLI().get_series_info(series_id)
         if args.get("-o"):
             outfile = args.get("-o")
-            
-            # make into a function 
-            if os.path.exists(outfile):
-                while True:
-                    print("File %s already exists. Do you want to overwrite it?" % outfile)
-                    print("y for yes / n for no, or c to save with a different name")
-                    user_choice = input("Please enter your choice or press enter now to quit")
-                    if user_choice == "":
-                        print("Quitting")
-                    elif user_choice == "y":
-                        with open(outfile, 'w') as f:
-                            f.writelines()  # double-check this
-                    elif user_choice == "n":
-                        break
-                    elif user_choice == "c":
-                        # recursive call
-                        pass 
-
+            check_user_input(outfile)
         print(series_info)
 
     elif args.get("latest"):
-        if not args.get("<series-id>"):
-            pass
+        check_id_param(args)
 
     elif args.get("configure"):
         pass
         # api key
+
+    elif args.get("vintage"):
+        check_id_param(args)
 
     elif args.get("examples"):
         pass
@@ -165,7 +158,7 @@ def cli():
 
     if args.get("<series-id>"):
         try: 
-            fred = Fred()
+            fred = FredCLI()
         except ValueError:
             pass
 
@@ -173,6 +166,7 @@ def cli():
         #       equivalent to $ fred info
 
     if args.get("s") or args.get("series"):
+        check_id_param(args, "<series-id>")
         print('args.get(series) works')
         # possible logic:
         # if retrieved previously:
@@ -183,21 +177,123 @@ def cli():
         # series has not been fetched at all yet: 
         #       fred.search() 
         if args.get("-asof"):
-            if not args.get("<series-id>"):
-                pass
+            pass
 
     elif args.get("search"):
         print('args.get(search) works')
-        if not args.get("<series-id>"):
-            while True:
-                user_choice = input(
-                "No series_id passed. Specify one now or press q to quit."
-                ).lower().strip()
-                if user_choice == "q":
-                    sys.exit(0)
-                    # return None?
-        fred.get_series()
+        # check:
+        #   text
+        #   limit
+        #   order by
+        #   sort order
+        #   filter
+        fred.search(**params)
+
+    elif args.get("sr") or args.get("search-release"):
+        check_id_param(args, "<release-id>")
+        # check:
+        #   release id
+        #   limit
+        #   order by
+        #   sort order
+        #   filter
 
     elif args.get("sc") or args.get("search-category"):
+        check_id_param(args, "<category-id>")
+        # check:
+        #   category id
+        #   limit
+        #   order by
+        #   sort order
+        #   filter
+        # check:
+        #   
         pass
+
+
+search_type_map = {
+        0: "full_text",
+        1: "release",
+        2: "category",
+        }
+
+def check_search_params(search_params, search_type: int = 0):
+    """
+    search type: 
+        0: search by category
+        1: search by release
+        2: full-text search
+    """
+    search_type = search_type_map[search_type]
+
+
+
+def check_id_param(args: dict, id_to_check: str):
+    """
+
+    """
+    if not args.get(id_to_check):
+        print("No %s passed" % id_to_check.strip("<").strip(">"))
+        sys.exit(1)
+    print('after sys.exit(1)')
+
+def check_user_input(outfile: str):
+    """
+    
+    """
+    if os.path.exists(outfile):
+        while True:
+            print("File %s already exists. Do you want to overwrite it?" % outfile)
+            print(" y for yes\n n for no \n c to choose a different name")
+            user_choice = input("Please enter your choice or press \
+                    enter now to quit").strip().lower()
+            if user_choice == "c":
+                check_user_input(user_choice)
+            if user_choice == "" or user_choice == "n":
+                print("Quitting")
+                sys.exit(0) # double-check that exit code should be 0
+            if user_choice == "y":
+                with open(outfile, 'w') as f:
+                    f.writelines()  # double-check this
+
+class FredCLI(Fred):
+    """
+    A subclass of fredapi.fred.Fred. Exceptions are modified to
+    be minimal and tailored for output to a terminal
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.__series_url_prefix = self.root_url + "/series?series_id="
+
+    def _fetch_data(self, url):
+        # override to raise quiet exceptions
+        # return bool to indicate failed operation and proceed from there, sidestepping
+        # verbose exception messages (verbose for terminal)
+        url += "&api_key=" + self.api_key
+
+        # test next 2 lines for possible exceptions to handle them quietly
+        try:
+            response = urlopen(url).read()
+            root = ET.fromstring(response)
+        except HTTPError:
+            return
+        return root
+
+    def get_series_info(self, series_id):
+        # override to raise quiet exceptions
+        # return bool to indicate failed operation and proceed from there, sidestepping
+        # verbose exception messages (verbose for terminal)
+        fetched_info = self._fetch_data(self.__series_url_prefix + series_id)
+        if not fetched_info:
+            print("Could not fetch info using series_id: %s" % series_id)
+            sys.exit(1) # ascertain whether this is better than returning None
+        return pd.Series(list(fetched_info)[0].attrib)
+
+
+
+
+
+
+
 
