5a17fb041fd0ec551afb1c9df86f79291f2be3eb
7astro7 <zaknyy@protonmail.com>
2021-03-25 19:05:28 -0700

    translate series queries into FredSeries class methods

---
+++ new_fred/tests/test_fred_class.py
@@ -0,0 +1,118 @@
+
+import pytest
+from new_fred.fred import Fred
+
+# ensure method coverage
+# test different realtime dates
+
+@pytest.fixture
+def fred():
+    return Fred()
+
+@pytest.fixture
+def expected_get_category_id_125():
+    expected = {
+        'categories': [
+            {'id': 125, 
+            'name': 'Trade Balance', 
+            'parent_id': 13}
+            ]
+        }
+    return expected
+
+@pytest.fixture
+def expected_get_series_id():
+    """
+    The expected value associated with key 'id' in the map returned
+    by fred.get_series('GNPCA')
+    """
+    return "GNPCA"
+
+@pytest.fixture
+def expected_get_series_title():
+    """
+    The expected value associated with key 'title' in the map returned
+    by fred.get_series('GNPCA')
+    """
+    return "Real Gross National Product"
+
+@pytest.mark.skip("passed")
+def test_get_category_id_125_returns_trade_balance(
+        fred: Fred, 
+        expected_get_category_id_125: dict,
+        ):
+    expected = expected_get_category_id_125 
+    assert fred.get_a_category(125) == expected
+
+@pytest.mark.skip("passed")
+def test_get_child_categories_id_13_returns_children_with_parentid_13(
+        fred: Fred, 
+        ):
+    """
+    New child categories may be added to the category subtree rooted 
+    at "U.S. Trade and Transactions" (category_id 13) and make this 
+    test fail despite fred.get_child_categories(13) returning the 
+    requested data. Instead of testing equivalence it makes more 
+    sense to test whether each category returned by the method call 
+    has parent_id of 13
+    """
+    returned_correctly = False
+    observed = fred.get_child_categories(13) 
+    for i in range(len(observed["categories"])):
+        a_category = observed["categories"][i]
+        if "parent_id" not in a_category.keys():
+            break
+        if a_category["parent_id"] != 13:
+            break
+        if i == len(observed["categories"]) - 1:
+            returned_correctly = True
+    assert returned_correctly == True
+
+def test_get_related_categories():
+    """
+    Unclear at this point how to test
+    """
+    pass
+
+
+def test_get_series_in_a_category_category_id_NO():
+    """
+    Unclear at this point how to test
+    """
+    pass
+
+def test_get_series(
+        fred: Fred,
+        expected_get_series_id: str,
+        expected_get_series_title: str,
+        ):
+    returned_correctly = False
+    observed = fred.get_series("GNPCA")
+    if "id" in observed.keys():
+        if not observed["id"] == expected_get_series_id:
+            assert returned_correctly == True
+    if "title" in observed.keys():
+        if not observed["title"] == expected_get_series_title: 
+            assert returned_correctly == True
+    returned_correctly = True
+    assert returned_correctly == True
+
+
+@pytest.mark.skip("not yet")
+def test_get_categories_of_series(
+        fred: Fred,
+        ):
+    returned_correctly = False
+    observed = fred.get_categories_of_series("EXJPUS")
+    if "id" in observed.keys():
+        if not observed["id"] == expected_get_series_id:
+            assert returned_correctly == True
+
+
+
+
+
+
+
+
+
