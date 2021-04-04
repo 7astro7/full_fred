64fbc85fb91bd7730e5f66d2ee99cd91c0cddea7
7astro7 <zaknyy@protonmail.com>
2021-04-03 18:27:52 -0700

    updates toml to use black, setup.py classifiers

--- setup.py
+++ setup.py
@@ -2,31 +2,27 @@
 from setuptools import setup
 import os
 
-path = os.path.join('new_fred', 'constants.py')
-def get_version():
-    version = None
-    with open(path, 'r') as f:
-        version = f.readlines()
-    return version
-
-
 setup(
         name = 'full_fred',
         packages = ['new_fred',],
-        version = get_version(),
+        version = '0.0.1.dev1',
         author = 'Zachary A. Kraehling',
         author_email = 'zaknyy@protonmail.com',
-        description = 'New, full interface to Federal Reserve Economic Data (FRED)',
+        description = 'Full interface to Federal Reserve Economic Data (FRED)',
         long_description = 'pass',
-        LICENSE = 'GPLv3',
+        license = 'GNU General Public License v3 (GPLv3)',
         install_requires = [],
         python_requires = '>=3.8',
         url = 'https://github.com/7astro7/full_fred',
-        test_suite = 'tests.test_fred_class',
+        test_suite = 'new_fred.tests',
         platforms = ['Any'],
         classifiers = [
-            'Development Status :: dev1',
-            'Environment :: Console',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Development Status :: 1 - Planning',
+            'Intended Audience :: Science/Research',
+            'Intended Audience :: Developers',
+            'Operating System :: Unix',
+            'Topic :: Software Development :: Libraries :: Python Modules',
             'Programming Language :: Python :: 3.8',
-            ], # add to this
+            ], 
         )
