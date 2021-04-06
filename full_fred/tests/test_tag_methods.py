1fcc6d4127f272f777ffca018608c39ff4fe29b5
7astro7 <zaknyy@protonmail.com>
2021-04-05 21:33:33 -0700

    adds all test_tag_methods successful tests v3

--- full_fred/tests/test_tag_methods.py
+++ full_fred/tests/test_tag_methods.py
@@ -8,33 +8,39 @@ def fred() -> Fred:
     return Fred()
 
 @pytest.fixture
-def get_all_tags_method_works(fred: Fred) -> bool:
+def returned_ok_params() -> dict:
+    return dict()
+
+@pytest.fixture
+def get_all_tags_method_works(
+        fred: Fred,
+        returned_ok_params: dict,
+        ) -> bool:
     params = {
             'limit': 2,
+            'realtime_start': '1950-12-19',
             'sort_order' : 'desc',
             'tag_names': ('gdp', 'oecd',),
             'order_by': "name",
             }
     fred.get_all_tags(**params)
-    observed = fred.tag_stack["get_all_tags"]
+    returned_ok_params['observed'] = fred.tag_stack["get_all_tags"]
     params.pop('tag_names')
-    expected = params
-    check_union = ('tags',)
-    returned_ok_params = {
-            'observed': observed,
-            'expected': expected,
-            'check_union': check_union,
-            }
+    returned_ok_params['expected'] = params
+    returned_ok_params['check_union'] = ('tags',)
     return returned_ok(**returned_ok_params)
 
-@pytest.mark.skip("passed v2")
+@pytest.mark.skip("passed v3")
 def test_get_all_tags(
         get_all_tags_method_works: bool,
         ):
     assert get_all_tags_method_works == True
 
 @pytest.fixture
-def get_related_tags_for_a_tag_method_works(fred: Fred) -> bool:
+def get_related_tags_for_a_tag_method_works(
+        fred: Fred,
+        returned_ok_params: dict,
+        ) -> bool:
     params = {
             'tag_names': ('monetary aggregates', 'weekly'),
             'limit': 2,
@@ -44,27 +50,25 @@ def get_related_tags_for_a_tag_method_works(fred: Fred) -> bool:
             'search_text': ('beans', 'cabbage',),
             }
     fred.get_related_tags_for_a_tag(**params)
-    observed = fred.tag_stack["get_related_tags_for_a_tag"]
+    returned_ok_params['observed'] = fred.tag_stack["get_related_tags_for_a_tag"]
     params.pop('tag_names')
     params.pop('tag_group_id')
     params.pop('search_text')
-    expected = params
-    check_union = ('tags',)
-    returned_ok_params = {
-            'observed': observed,
-            'expected': expected,
-            'check_union': check_union,
-            }
+    returned_ok_params['expected'] = params
+    returned_ok_params['check_union'] = ('tags',)
     return returned_ok(**returned_ok_params)
 
-@pytest.mark.skip("passed v2")
+@pytest.mark.skip("passed v3")
 def test_get_related_tags_for_a_tag(
         get_related_tags_for_a_tag_method_works: bool,
         ):
     assert get_related_tags_for_a_tag_method_works == True
 
 @pytest.fixture
-def get_series_matching_tags_method_works(fred: Fred) -> bool:
+def get_series_matching_tags_method_works(
+        fred: Fred,
+        returned_ok_params: dict,
+        ) -> bool:
     params = {
             'tag_names': ('slovenia', 'food', 'oecd',),
             'limit': 2,
@@ -72,21 +76,16 @@ def get_series_matching_tags_method_works(fred: Fred) -> bool:
             'order_by': 'seasonal_adjustment',
             'sort_order': 'desc',
 #            'realtime_start': '2000-01-01',
-#            'realtime_end': '2003-01-01',
+            'realtime_end': '2003-01-01',
             }
     fred.get_series_matching_tags(**params)
-    observed = fred.tag_stack["get_series_matching_tags"]
+    returned_ok_params['observed'] = fred.tag_stack["get_series_matching_tags"]
     params.pop('tag_names')
-    expected = params
-    check_union = ('series', 'seriess',)
-    returned_ok_params = {
-            'observed': observed,
-            'expected': expected,
-            'check_union': check_union,
-            }
+    returned_ok_params['expected'] = params
+    returned_ok_params['check_union'] = ('series', 'seriess',)
     return returned_ok(**returned_ok_params)
 
-@pytest.mark.skip("passed v2")
+@pytest.mark.skip("passed v3")
 def test_get_series_matching_tags(
         get_series_matching_tags_method_works: bool,
         ):
